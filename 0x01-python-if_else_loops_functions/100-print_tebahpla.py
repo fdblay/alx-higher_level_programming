f73003b32a0d24e039fc2daf6387b30ad169d04d
fdblay <fdblay@gmail.com>
2023-01-10 10:45:53 +0000

    advance tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Print the alphabet in reverse order alternating lower and uppercase"""
 i = 0
-for letter in range(ord('z'), ord('a') -1, -1):
+for letter in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(letter - i)), end="")
     i = 32 if i == 0 else 0
