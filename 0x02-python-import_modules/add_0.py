d99b391f4dc2004e0157a0fc62d7256e78d698cc
fdblay <fdblay@gmail.com>
2023-01-12 19:07:47 +0000

    import add()

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Function adds two integers
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
