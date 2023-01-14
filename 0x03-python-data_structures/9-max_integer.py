cf1fb8acf890dafe69a261c99d5930fb6e211a82
fdblay <fdblay@gmail.com>
2023-01-14 20:50:20 +0000

    max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """Finds the biggest integer of a list"""
-    if not my_list:
+    if not my_list or len(my_list) == 0:
         return None
-    biggest = 0
+    biggest = my_list[0]
     for index in my_list:
         if index > biggest:
             biggest = index
