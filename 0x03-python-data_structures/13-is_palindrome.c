bb4fdfd32fd5fa624dfc4bf3c867c08df8052d2e
fdblay <fdblay@gmail.com>
2023-01-15 14:24:18 +0000

    is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,17 +6,45 @@
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
 
-	if (head == NULL|| *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
 
-	temp = *head;
-	while (temp) /* get len of list */
+	if (p_check(head, *head))
 	{
-		temp = temp->next;
-		len++;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * p_check - check for palindrome
+ * @left: Go left
+ * @right: Go right
+ *
+ * Return: integer
+ */
+
+int p_check(listint_t **left, listint_t *right)
+{
+	int is_p = 0;
+
+	if (right)
+	{
+		is_p = p_check(left, right->next);
+	}
+	else
+	{
+		return (1);
+	}
+
+	if (is_p == 1)
+	{
+		if ((*left)->n == right->n)
+		{
+			(*left) = (*left)->next;
+			return (1);
+		}
 	}
 	return (0);
 }
