49dfcbbcd1f7a4ec456820571ccb3935ec48e209
fdblay <fdblay@gmail.com>
2023-01-24 15:39:31 +0000

    all

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    printed = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
-            printed += 1
-        except:
+            print(f'{my_list[i]}', end="")
+            i += 1
+        except (IndexError):
             continue
     print()
-    return printed
+    return (i)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,9 +7,11 @@ def safe_print_division(a, b):
     catches divide by zero exception
     """
     try:
-        res = a / b
-    except:
-        res = None
+        return (a / b)
+    except ZeroDivisionError:
+        return (None)
     finally:
-        print("Inside result: {}".format(res))
-    return res
+        if b != 0:
+            print("Inside result: {}".format(a / b))
+        else:
+            print("Inside result: None")
