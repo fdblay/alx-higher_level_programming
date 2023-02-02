fcaab32b904666870c3f94049c70e41cb2c6af02
fdblay <fdblay@gmail.com>
2023-02-02 17:30:08 +0000

    empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-""" An empty class, Rectangle"""
+"""0-rectangle, built for ALX/Holberton project 0x08 task 0.
+"""
+
+
 class Rectangle:
+    """Empty class, Rectangle. Will be built upon
+    """
     pass
