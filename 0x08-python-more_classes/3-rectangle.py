6a942a5ac5150260754ed2779d3e27248a394f1e
fdblay <fdblay@gmail.com>
2023-02-03 11:25:36 +0000

    3-rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -106,15 +106,16 @@ class Rectangle:
 
     def _draw_rectangle(self):
         """Formats a string of '#' and '\n' chars to print the rectangle
-        by the current instance.
+        represented by the current instance.
 
         Attributes:
             __width (int): horizontal dimension of rectangle
             __height (int): vertical dimension of rectangle
-            str (str): string to be constructed for return
+            str (str): string to constructed for return
 
         Returns:
-            str (str): string suitable for printing rectangle (final newline omitted)
+            str (str): string suitable for printing rectangle (final newline
+            omitted)
 
         """
         str = ""
@@ -130,7 +131,7 @@ class Rectangle:
 
         Returns:
             The output of _draw_rectangle, which creates a string
-            representation of the rectangle suitable for printing.
+        representation of the rectangle suitable for printing.
 
         """
         return self._draw_rectangle()
