e513a804898a1bf62ff282146830559419868baa
fdblay <fdblay@gmail.com>
2023-02-11 13:20:23 +0000

    3 and 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#4-inherits_from.py
+"""Defines an inheriter class-verifying function."""
+
+
+def inherits_from(obj, a_class):
+    """Verify if an object is an inherited instance of a class.
+    Args:
+        obj (any): The object to verify.
+        a_class (type): The class to match the type of object to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
