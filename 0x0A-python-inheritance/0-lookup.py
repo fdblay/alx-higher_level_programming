426b1cb5157e6510022d99121d6e6303c0c6dca4
fdblay <fdblay@gmail.com>
2023-02-11 11:31:28 +0000

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-#0-lookup.py
-"""Defines an object attribute, lookup function. Task 0"""
+# 0-lookup.py
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """Returns the list of object's available attributes && methods"""
+    """Return a list of an object's available attributes."""
     return (dir(obj))
