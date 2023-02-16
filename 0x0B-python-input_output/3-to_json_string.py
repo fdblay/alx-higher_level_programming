bfefc653454c8af9a3620536d7a56df951e2b6ac
fdblay <fdblay@gmail.com>
2023-02-16 14:38:40 +0000

    to json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Defines the function to_json_string(...)"""
 import json
 
+
 def to_json_string(my_obj):
     """Returns the JSON representation of an object(sring)"""
     return json.dumps(my_obj)
