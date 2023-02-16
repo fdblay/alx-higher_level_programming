aa9902ee598b78ae729115f7cfae62d0c5def8a4
fdblay <fdblay@gmail.com>
2023-02-16 11:19:53 +0000

    1-write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python Input_Output, ALX Task-0, "1-write_file.py"""
+
+
+def write_file(filename="", text=""):
+    """Function writes a string to a textfile (UTF8)
+    Args:
+        filename (str): The name of the file to read from
+        text (str): The string to be written
+    Returns:
+        the number of characters written
+    """
+    with open(filename,"w", encoding='UTF-8') as w_file:
+        w_file.write(text)
+        return len(text)
