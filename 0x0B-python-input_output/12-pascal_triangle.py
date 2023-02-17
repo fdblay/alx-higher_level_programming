2085d92db02af0aca93b0dbd18ca7ada473a5d1d
fdblay <fdblay@gmail.com>
2023-02-17 11:45:13 +0000

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a function called pascal_triangle(...)"""
+"""Contains a function called pascal_triangle(...)"""
 
 
 def pascal_triangle(n):
-    """returns a list of lists of integers representing pascal's 
-    triangle of n"""
+    """returns a list of lists of integers representing pascal's triangle
+    of n"""
     result = []
 
     if n <= 0:
